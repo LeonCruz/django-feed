c0d510d4f34d7f89b2ec4a521d7030a9b25789ef
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-03 19:42:38 -0300

    Exibição dos links vindos do RSS passado no formulário

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from feedparser import parse
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/index.html')
+class Index(View):
+    def get(self, request):
+        return render(request, 'home/index.html')
+
+    def post(self, request):
+        rss_site = request.POST['rss_site']
+        data = get_rss_fedd(rss_site)
+
+        return render(request, 'home/index.html', {'data': data})
+
+
+def get_rss_fedd(url):
+    feed = parse(url)
+    entries = feed['entries']
+
+    data = [(entrie['title'], entrie['link']) for entrie in entries]
+
+    return data
